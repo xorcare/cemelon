289690d37ada9cf55cb7eee6939a8d7c6626a139
Vasilyuk Vasiliy <vasilyukvasiliy@gmail.com>
2018-01-08 14:43:23 +0400

    feat(*): A new approach to bypass blocks
    Added bypass blocks with the help of pocket including my design, a modified procedure of data recording in files improved parallel data processing and filtering of duplicate addresses.

--- README.md
+++ README.md
@@ -22,9 +22,50 @@ Usage of ./cemelon:
   -z    Not to collect all addresses
 ~~~
 
+## Dependencies
+
+Install go packages:
+
+```bash
+go get github.com/vasilyukvasiliy/blockchain
+```
+
 ## Scripts
 
  * **top100bitcoin-richest.sh** - Allows you to collect top 10,000 richest Bitcoin and Bitcoin Cash addresses
+
+## Example of the execution log
+
+```log
+#######################################################
+#                                                     #
+#   #####                                             #
+#  #     # ###### #    # ###### #       ####  #    #  #
+#  #       #      ##  ## #      #      #    # ##   #  #
+#  #       #####  # ## # #####  #      #    # # #  #  #
+#  #       #      #    # #      #      #    # #  # #  #
+#  #     # #      #    # #      #      #    # #   ##  #
+#   #####  ###### #    # ###### ######  ####  #    #  #
+#                                                     #
+#  The MIT License (MIT)                              #
+#  Copyright 2017-2018 Vasilyuk Vasiliy               #
+#  Email: vasilyukvasiliy@gmail.com                   #
+#######################################################
+#            Github: https://git.io/vNIKR             #
+#######################################################
+
+
+cemelon -s 300000 -e 300010 -n 2
+Mon, 01 Jan 2018 10:31:48 UTC | Block index:  300000
+Mon, 01 Jan 2018 10:31:48 UTC | Block index:  300005
+Mon, 01 Jan 2018 10:31:49 UTC | Block index:  300006
+Mon, 01 Jan 2018 10:31:50 UTC | Block index:  300007
+Mon, 01 Jan 2018 10:31:50 UTC | Block index:  300001
+Mon, 01 Jan 2018 10:31:53 UTC | Block index:  300008
+Mon, 01 Jan 2018 10:31:54 UTC | Block index:  300009
+Mon, 01 Jan 2018 10:31:56 UTC | Block index:  300010
+```
+
 
 ## License
 
--- cemelon.go
+++ cemelon.go
@@ -1,275 +1,272 @@
 package main
 
 // The MIT License (MIT)
-// Copyright 2017 Vasilyuk Vasiliy <vasilyukvasiliy@gmail.com>
+// Copyright 2017-2018 Vasilyuk Vasiliy <vasilyukvasiliy@gmail.com>
 
 import (
-	"errors"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"regexp"
-	"strconv"
-	"sync"
-	"time"
+    "flag"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+    "sync"
+    "time"
+
+    "github.com/vasilyukvasiliy/blockchain"
 )
 
 var (
-	StartBlockIndex               int    = -1
-	EndBlockIndex                 int    = -1
-	countStreams                  int    = 1
-	whitenAddressSize             int    = 1000000
-	NotCollectFirstAddresses      bool   = false
-	NotCollectAllAddresses        bool   = false
-	FirstAddressesInBlockFileName string = "frs-cemelon-addresses.txt"
-	AllAddressesInBlockFileName   string = "all-cemelon-addresses.txt"
-	isWhitenAddress               Map
+    startBlockIndex          = -1
+    endBlockIndex            = -1
+    countStreams             = 1
+    whitenAddressSize        = 262144
+    notCollectFirstAddresses = false
+    notCollectAllAddresses   = false
+    OutFileBaseName          = "cemelon.txt"
+    isWhitenAddress          Map
 )
 
 func init() {
-	flag.IntVar(&StartBlockIndex, "s", StartBlockIndex,
-		"The block number at which to start collecting addresses")
+    fmt.Println(`
+#######################################################
+#                                                     #
+#   #####                                             #
+#  #     # ###### #    # ###### #       ####  #    #  #
+#  #       #      ##  ## #      #      #    # ##   #  #
+#  #       #####  # ## # #####  #      #    # # #  #  #
+#  #       #      #    # #      #      #    # #  # #  #
+#  #     # #      #    # #      #      #    # #   ##  #
+#   #####  ###### #    # ###### ######  ####  #    #  #
+#                                                     #
+#  The MIT License (MIT)                              #
+#  Copyright 2017-2018 Vasilyuk Vasiliy               #
+#  Email: vasilyukvasiliy@gmail.com                   #
+#######################################################
+#            Github: https://git.io/vNIKR             #
+#######################################################
 
-	flag.IntVar(&EndBlockIndex, "e", EndBlockIndex,
-		"The block number on which program finished collecting the addresses including this number")
+`)
 
-	flag.IntVar(&countStreams, "n", countStreams,
-		"The number of threads downloading data")
+    fmt.Println(os.Args)
 
-	flag.IntVar(&whitenAddressSize, "m", whitenAddressSize,
-		"The number of addresses stored in the card to prevent re-entry of addresses")
+    flag.IntVar(&startBlockIndex, "s", startBlockIndex, "The block number at which to start collecting addresses")
+    flag.IntVar(&endBlockIndex, "e", endBlockIndex, "The block number on which program finished collecting the addresses including this number")
+    flag.IntVar(&whitenAddressSize, "m", whitenAddressSize, "The number of addresses stored in the card to prevent re-entry of addresses")
+    flag.BoolVar(&notCollectFirstAddresses, "r", notCollectFirstAddresses, "Not to collect the first address in the block")
+    flag.IntVar(&countStreams, "n", countStreams, "The number of threads downloading data")
+    flag.StringVar(&OutFileBaseName, "o", OutFileBaseName, "Output data file base name")
+    flag.BoolVar(&notCollectAllAddresses, "z", notCollectAllAddresses, "Not to collect all addresses")
+    flag.Parse()
 
-	flag.StringVar(&FirstAddressesInBlockFileName, "f", FirstAddressesInBlockFileName,
-		"The name of the file which will be written to the first address in the block")
-
-	flag.StringVar(&AllAddressesInBlockFileName, "a", AllAddressesInBlockFileName,
-		"The name of the file which will be used to record all addresses in the block")
-
-	flag.BoolVar(&NotCollectFirstAddresses, "r", NotCollectFirstAddresses,
-		"Not to collect the first address in the block")
-
-	flag.BoolVar(&NotCollectAllAddresses, "z", NotCollectAllAddresses,
-		"Not to collect all addresses")
-
-	flag.Parse()
-
-	isWhitenAddress = *NewMap()
+    isWhitenAddress = *NewMap()
 }
 
 type Map struct {
-	mx sync.Mutex
-	m  map[string]bool
+    mx sync.Mutex
+    m  map[string]bool
 }
 
 func (c *Map) Count() int {
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	return len(c.m)
+    c.mx.Lock()
+    defer c.mx.Unlock()
+    return len(c.m)
 }
 
 func (c *Map) Exist(key string) bool {
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	_, ok := c.m[key]
-	return ok
+    c.mx.Lock()
+    defer c.mx.Unlock()
+    _, ok := c.m[key]
+    return ok
 }
 
 func (c *Map) Store(key string, value bool) {
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	c.m[key] = value
+    c.mx.Lock()
+    defer c.mx.Unlock()
+    c.m[key] = value
 }
 
 func (c *Map) Clear() {
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	c.m = map[string]bool{}
+    c.mx.Lock()
+    defer c.mx.Unlock()
+    c.m = map[string]bool{}
 }
 
 func NewMap() *Map {
-	return &Map{
-		m: make(map[string]bool),
-	}
+    return &Map{
+        m: make(map[string]bool),
+    }
 }
 
 type InformationRecord struct {
-	Filename   string
-	Message    string
-	BlockIndex int
+    Filename   string
+    Message    string
+    BlockIndex int
 }
 
 func Write2FileFromChan(cn <-chan InformationRecord, wg *sync.WaitGroup) {
-	var files map[string]*os.File = map[string]*os.File{}
-	var err error = nil
-	for {
-		dan := <-cn
-		wg.Add(1)
-		for counter := 0; counter <= 64; counter++ {
-			if files[dan.Filename] == nil {
-				files[dan.Filename], err =
-					os.OpenFile(dan.Filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-				if err != nil {
-					files[dan.Filename] = nil
-					fmt.Fprintln(os.Stderr, nowTime(), "|", "Block index: ", dan.BlockIndex, "[Error]", err)
-					continue
-				}
-				defer files[dan.Filename].Close()
-			}
-			_, err = fmt.Fprintln(files[dan.Filename], dan.Message)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, nowTime(), "|", "Block index: ", dan.BlockIndex, "[Error]", err)
-			} else {
-				break
-			}
-			if counter == 64 {
-				log.Fatalln(nowTime(), "|", "Block index: ", dan.BlockIndex, "[Error]", err)
-			}
-			time.Sleep(time.Millisecond)
-		}
-		wg.Done()
-	}
+    var files = map[string]*os.File{}
+    var err error = nil
+
+    pid := strconv.Itoa(os.Getpid())
+    for {
+        dan := <-cn
+        dan.Filename = pid + "-" + dan.Filename
+        for counter := 0; counter <= 64; counter++ {
+            if files[dan.Filename] == nil {
+                files[dan.Filename], err =
+                    os.OpenFile(dan.Filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+                if err != nil {
+                    files[dan.Filename] = nil
+                    fmt.Fprintln(os.Stderr, nowTimeRFC1123(), "|", "Block index: ", dan.BlockIndex, "[Error]", err)
+                    continue
+                }
+                defer files[dan.Filename].Close()
+            }
+            _, err = fmt.Fprintln(files[dan.Filename], dan.Message)
+            if err != nil {
+                fmt.Fprintln(os.Stderr, nowTimeRFC1123(), "|", "Block index: ", dan.BlockIndex, "[Error]", err)
+            } else {
+                break
+            }
+            if counter == 64 {
+                log.Fatalln(nowTimeRFC1123(), "|", "Block index: ", dan.BlockIndex, "[Error]", err)
+            }
+            time.Sleep(time.Millisecond)
+        }
+    }
 }
 
 func main() {
-	if EndBlockIndex < 0 || StartBlockIndex < 0 || (EndBlockIndex-StartBlockIndex) < 0 {
-		flag.Usage()
-		os.Exit(0)
-	}
+    if endBlockIndex < 0 || startBlockIndex < 0 || (endBlockIndex-startBlockIndex) < 0 {
+        flag.Usage()
+        os.Exit(0)
+    }
 
-	count := EndBlockIndex - StartBlockIndex
-	step := int(count / countStreams)
-	var wg sync.WaitGroup
-	var writer chan InformationRecord = make(chan InformationRecord, 32*countStreams)
+    count := endBlockIndex - startBlockIndex
+    step := int(count / countStreams)
+    var wg sync.WaitGroup
+    chanInformationRecords := make(chan InformationRecord, 32*countStreams)
 
-	go Write2FileFromChan(writer, &wg)
+    go Write2FileFromChan(chanInformationRecords, &wg)
 
-	if count > 0 && step > 1 {
-		for i := StartBlockIndex; i <= EndBlockIndex; i += step {
-			end := i + step - 1
-			start := i
-			if end > EndBlockIndex || (EndBlockIndex-end) < step {
-				end = EndBlockIndex
-				i = EndBlockIndex
-			}
-			go worker(&wg, writer, start, end)
-		}
-	} else {
-		go worker(&wg, writer, StartBlockIndex, EndBlockIndex)
-	}
+    if count > 0 && step > 1 {
+        for i := startBlockIndex; i <= endBlockIndex; i += step {
+            end := i + step - 1
+            start := i
+            if end > endBlockIndex || (endBlockIndex-end) < step {
+                end = endBlockIndex
+                i = endBlockIndex
+            }
+            go worker(&wg, chanInformationRecords, start, end)
+        }
+    } else {
+        go worker(&wg, chanInformationRecords, startBlockIndex, endBlockIndex)
+    }
 
-	for {
-		time.Sleep(time.Second)
-		wg.Wait()
+    time.Sleep(time.Second)
+    wg.Wait()
 
-		if len(writer) == 0 {
-			break
-		}
-	}
+    for {
+        time.Sleep(time.Second)
+        if len(chanInformationRecords) == 0 {
+            break
+        }
+    }
+
+    time.Sleep(time.Second)
 }
 
 func worker(wg *sync.WaitGroup, cn chan<- InformationRecord, startIndex, endIndex int) {
-	wg.Add(1)
-	defer wg.Done()
+    wg.Add(1)
+    defer wg.Done()
 
-	var (
-		err               error
-		blockIndexInt     int    = 0
-		blockIndexStr     string = ""
-		prevBlockIndexInt int    = -1
-		jsonDataString    string = ""
-		isDone            bool   = true
-	)
+    var block *blockchain.Block = nil
+    var (
+        blockIndexInt     = 0
+        blockIndexStr     = ""
+        prevBlockIndexInt = -1
+        isDone            = true
+    )
 
-	blockIndexInt = startIndex
-	for blockIndexInt <= endIndex {
-		blockIndexStr = strconv.Itoa(blockIndexInt)
-		fmt.Fprintln(os.Stdout, nowTime(), "|", "Block index: ", blockIndexInt)
+    blc := blockchain.New()
+    blockIndexInt = startIndex
+    for blockIndexInt <= endIndex {
+        blockIndexStr = strconv.Itoa(blockIndexInt)
+        fmt.Fprintln(os.Stdout, nowTimeRFC1123(), "|", "Block index: ", blockIndexInt)
 
-		if prevBlockIndexInt != blockIndexInt {
-			jsonDataString = ""
-		}
+        if prevBlockIndexInt != blockIndexInt {
+            block = nil
+        }
 
-		if jsonDataString == "" {
-			urlString := "https://blockchain.info/block-height/" + blockIndexStr + "?format=json"
-			jsonDataString, err = GetDataStringByUrl(urlString)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, nowTime(), "|", "Block index: ", blockIndexInt, "[Error]", err)
-				jsonDataString = ""
-				time.Sleep(time.Second * 16)
-				continue
-			}
-		}
+        if block == nil {
+            res, err := blc.GetBlockHeight(blockIndexStr)
+            if err != nil {
+                fmt.Fprintln(os.Stderr, nowTimeRFC1123(), "|", "Block index: ", blockIndexInt, "[Error]", err)
+                block = nil
+                time.Sleep(time.Second * 16)
+                continue
+            }
+            block = &res.Blocks[0]
+        }
 
-		regex := regexp.MustCompile("\"addr\":\"([13][a-km-zA-HJ-NP-Z1-9]{25,34})\"")
-		addresses := regex.FindAllStringSubmatch(jsonDataString, -1)
+        if len(block.Tx) < 1 {
+            fmt.Fprintln(os.Stderr, nowTimeRFC1123(), "|", "Not found address, block index:", blockIndexInt)
+            block = nil
+            time.Sleep(time.Second * 16)
+            continue
+        }
 
-		if len(addresses) < 1 {
-			fmt.Fprintln(os.Stderr, nowTime(), "|", "Not found address, block index:", blockIndexInt)
-			jsonDataString = ""
-			time.Sleep(time.Second * 16)
-			continue
-		}
+        addresses := make([]string, 0, 0)
+        for _, tx := range block.Tx {
+            for _, out := range tx.Out {
+                addresses = append(addresses, out.Addr)
+            }
+        }
 
-		isDone = true
-		for j, num := range addresses {
-			if j == 0 && !isWhitenAddress.Exist(blockIndexStr) && !NotCollectFirstAddresses {
-				cn <- InformationRecord{
-					Filename:   FirstAddressesInBlockFileName,
-					Message:    num[1],
-					BlockIndex: blockIndexInt,
-				}
-				isWhitenAddress.Store(blockIndexStr, true)
-			}
+        if !isWhitenAddress.Exist(block.Hash) {
+            cn <- InformationRecord{
+                Filename:   "blk-" + OutFileBaseName,
+                Message:    block.Hash,
+                BlockIndex: blockIndexInt,
+            }
+            isWhitenAddress.Store(block.Hash, true)
+        }
 
-			if NotCollectAllAddresses {
-				isDone = true
-				break
-			}
+        isDone = true
+        for j, address := range addresses {
+            if j == 0 && !isWhitenAddress.Exist(blockIndexStr) && !notCollectFirstAddresses {
+                cn <- InformationRecord{
+                    Filename:   "frs-" + OutFileBaseName,
+                    Message:    address,
+                    BlockIndex: blockIndexInt,
+                }
+                isWhitenAddress.Store(blockIndexStr, true)
+            }
 
-			if !isWhitenAddress.Exist(num[1]) {
-				cn <- InformationRecord{
-					Filename:   AllAddressesInBlockFileName,
-					Message:    num[1],
-					BlockIndex: blockIndexInt,
-				}
-				isWhitenAddress.Store(num[1], true)
-			}
-		}
+            if notCollectAllAddresses {
+                isDone = true
+                break
+            }
 
-		if isDone {
-			blockIndexInt++
-			if isWhitenAddress.Count() > whitenAddressSize {
-				isWhitenAddress.Clear()
-			}
-		}
-	}
+            if !isWhitenAddress.Exist(address) {
+                cn <- InformationRecord{
+                    Filename:   "all-" + OutFileBaseName,
+                    Message:    address,
+                    BlockIndex: blockIndexInt,
+                }
+                isWhitenAddress.Store(address, true)
+            }
+        }
+
+        if isDone {
+            blockIndexInt++
+            if isWhitenAddress.Count() > whitenAddressSize {
+                isWhitenAddress.Clear()
+            }
+        }
+    }
 }
 
-func GetDataStringByUrl(url string) (s string, e error) {
-	s = ""
-	c := &http.Client{Transport: &http.Transport{}}
-	response, e := c.Get(url)
-	if e != nil {
-		return
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode < http.StatusOK || response.StatusCode > http.StatusIMUsed {
-		return s, errors.New("GetDataStringByUrl: Bad response status code!")
-	}
-
-	responseBody, e := ioutil.ReadAll(response.Body)
-	if e != nil {
-		return
-	}
-
-	s = string(responseBody)
-	return
-}
-
-func nowTime() string {
-	return time.Now().UTC().Format(time.RFC1123)
+func nowTimeRFC1123() string {
+    return time.Now().UTC().Format(time.RFC1123)
 }
