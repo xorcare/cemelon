472bd3e37f7a47fe11254d7c761a34f5af856f05
Vasilyuk Vasiliy <vasilyukvasiliy@gmail.com>
2018-01-19 00:02:32 +0400

    feat(*): Updated RagExp patterns

--- scripts/largest-recent-transactions.sh
+++ scripts/largest-recent-transactions.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-patter="([13][a-km-zA-HJ-NP-Z1-9]{25,34})"
+RegExp="[13][a-km-zA-HJ-NP-Z1-9]{25,34}"
+RegExpFilter="[13][a-km-zA-HJ-NP-Z1-9]{1,}[A-HJ-NP-Z]{1,}[a-km-zA-HJ-NP-Z1-9]{1,}"
+ServiceAddress="https://blockchain.info/largest-recent-transactions"
 
 TM=$(date +%Y-%m-%d_%H-%M-%S)
 LRT="largest-recent-transactions-"${TM}
 
-rm *.tmp.txt
-
-curl https://blockchain.info/largest-recent-transactions | egrep -o -e "${patter}" | sort -u >> ${LRT}.tmp.txt
+curl ${ServiceAddress} | egrep -o -e "${RegExp}" | egrep -e "${RegExpFilter}" | sort -u >> ${LRT}.tmp.txt
 cat ${LRT}.tmp.txt | sort -u > ${LRT}.txt
 
-rm *.tmp.txt
+rm ${LRT}.tmp.txt
--- scripts/top100bitcoin-richest.sh
+++ scripts/top100bitcoin-richest.sh
@@ -2,19 +2,20 @@
 
 BTC="bitcoin"
 BTH="bitcoin%20cash"
-patter="([13][a-km-zA-HJ-NP-Z1-9]{25,34})"
+RegExp="[13][a-km-zA-HJ-NP-Z1-9]{25,34}"
+RegExpFilter="[13][a-km-zA-HJ-NP-Z1-9]{1,}[A-HJ-NP-Z]{1,}[a-km-zA-HJ-NP-Z1-9]{1,}"
 
-curl https://bitinfocharts.com/ru/top-100-richest-${BTC}-addresses.html | egrep -o -e "${patter}" | sort -u >> ${BTC}.tmp.txt
-curl https://bitinfocharts.com/ru/top-100-richest-${BTH}-addresses.html | egrep -o -e "${patter}" | sort -u >> ${BTH}.tmp.txt
+curl https://bitinfocharts.com/ru/top-100-richest-${BTC}-addresses.html | egrep -o -e "${RegExp}" | egrep -o -e "${RegExpFilter}" | sort -u >> ${BTC}.tmp.txt
+curl https://bitinfocharts.com/ru/top-100-richest-${BTH}-addresses.html | egrep -o -e "${RegExp}" | egrep -o -e "${RegExpFilter}" | sort -u >> ${BTH}.tmp.txt
 
 for (( i=1; i <= 101; i++ ))
 do
-curl https://bitinfocharts.com/ru/top-100-richest-${BTC}-addresses-${i}.html | egrep -o -e "${patter}" | sort -u >> ${BTC}.tmp.txt
-curl https://bitinfocharts.com/ru/top-100-richest-${BTH}-addresses-${i}.html | egrep -o -e "${patter}" | sort -u >> ${BTH}.tmp.txt
+curl https://bitinfocharts.com/ru/top-100-richest-${BTC}-addresses-${i}.html | egrep -o -e "${RegExp}" |  egrep -o -e "${RegExpFilter}" |sort -u >> ${BTC}.tmp.txt
+curl https://bitinfocharts.com/ru/top-100-richest-${BTH}-addresses-${i}.html | egrep -o -e "${RegExp}" |  egrep -o -e "${RegExpFilter}" |sort -u >> ${BTH}.tmp.txt
 done
 
 TM=$(date +%Y-%m-%d_%H-%M-%S)
 cat ${BTC}.tmp.txt | sort -u > top-10000-richest-bitcoin-addresses-${TM}.txt
 cat ${BTH}.tmp.txt | sort -u > top-10000-richest-bitcoin-cash-addresses-${TM}.txt
 
-rm *.tmp.txt
+rm ${BTH}.tmp.txt ${BTC}.tmp.txt
